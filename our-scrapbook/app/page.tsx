88c1502dae627dd4ff079b421335944ee0c18e09
derrickhayward14 <57364467+derrickhayward14@users.noreply.github.com>
2026-02-07 03:31:37 +0000

    Create Next.js app

---
+++ our-scrapbook/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-4xl font-semibold">
+      Our Scrapbook 💕
+    </main>
+  );
+}
